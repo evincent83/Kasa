2672a826e6bb9acf5fd46c5763bb593596414409
evincent83 <85114547+evincent83@users.noreply.github.com>
2022-12-05 15:12:31 +0100

    update width / height

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,19 +1,24 @@
-import {useState} from 'react';
-import vector from '../img/vector.png';
+import { useState } from "react";
+import vector from "../img/vector.png";
 
-export default function Dropdown (props) {
-    const [open, setOpen] = useState(false);
-    return (
-        <>
-        <div className="dropdown_menu">
-            <div className="dropdown_title">{props.title}</div>
-            <div className="dropdown_toogle" onClick={()=>{setOpen(!open)}}>
-                <img className={`vector_state ${open? 'active' : 'inactive'}`} src={vector}></img>
-            </div>
+export default function Dropdown(props) {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="dropdown_container">
+      <div className="dropdown_menu">
+        <div className="dropdown_title">{props.title}</div>
+        <div
+          className="dropdown_toogle"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <img className={`vector_state ${open ? "active" : "inactive"}`} alt="vector" src={vector}></img>
         </div>
-        <div className={`dropdown_info ${open? 'active' : 'inactive'}`}>
-            <p>{props.dp_text}</p>
-        </div>
-        </>
-    )
+      </div>
+      <div className={`dropdown_info ${open ? "active" : "inactive"}`}>
+        <p>{props.text}</p>
+      </div>
+    </div>
+  );
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,10 +7,10 @@ export default function About() {
     <main>
       <ImgSubHeader backImg={backImg} alt={"Background Kasa About"}/> 
       <section id="dropdown_a">
-        <Dropdown title="Fiabilité" dp_text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."/>
-        <Dropdown title="Respect" dp_text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
-        <Dropdown title="Service" dp_text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
-        <Dropdown title="Sécurité" dp_text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
+        <Dropdown title="Fiabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."/>
+        <Dropdown title="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+        <Dropdown title="Service" text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
+        <Dropdown title="Sécurité" text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
       </section>
     </main>
   );
