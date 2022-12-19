e79303e330974063a435ebe4f3880a6013e911b0
evincent83 <85114547+evincent83@users.noreply.github.com>
2022-12-20 00:56:51 +0100

    prettier thumbgrid format

--- src/components/thumbGrid.jsx
+++ src/components/thumbGrid.jsx
@@ -2,14 +2,11 @@ import data from "../datas/logements.json";
 import Thumb from "../components/thumb";
 
 export default function ThumbGrid() {
-
-  const dataList = data.map((data) => {return {id: data.id, title: data.title, cover: data.cover}});
-  const grid = dataList.map((data) => 
-  <Thumb id={data.id} key={data.id} titleThumb={data.title} imgThumb={data.cover} altThumb={data.title}/>
-  ); 
-  return (
-      <section id="sectionThumb">
-        {grid}
-    </section>
-  );
+  const dataList = data.map((data) => {
+    return { id: data.id, title: data.title, cover: data.cover };
+  });
+  const grid = dataList.map((data) => (
+    <Thumb id={data.id} key={data.id} titleThumb={data.title} imgThumb={data.cover} altThumb={data.title} />
+  ));
+  return <section id="sectionThumb">{grid}</section>;
 }
