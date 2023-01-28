a89d0153c6d27428077d05b6870447a9a8576a8f
evincent83 <85114547+evincent83@users.noreply.github.com>
2023-01-28 14:01:32 +0100

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/kasa/build">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
