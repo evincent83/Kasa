ccfe7db53b598ecbc9030d61f29226b182f70057
evincent83 <85114547+evincent83@users.noreply.github.com>
2023-02-07 00:49:34 +0100

    try to replace BrowserRouter => HashRouter for gh

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/app.css";
 import 'normalize.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
